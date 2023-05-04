9b8b747b665b24d97c5129b9a0be15ee15162796
Dengwen-Fu <42565411+Dengwen-Fu@users.noreply.github.com>
2023-05-05 01:13:35 +0800

    update

--- MainProgram.py
+++ MainProgram.py
@@ -5,9 +5,9 @@ import fileinput
 import time
 import random
 
-
+requests.adapters.DEFAULT_RETRIES = 5
 headers = {
-    'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36','Connection':'close'
 }
 
 ##The following function can be used to delete when the number of rows is too large.
@@ -17,7 +17,7 @@ def sumline():
     for line in fileinput.input(r'.\MyICMGrades.html'):
         lineno=fileinput.filelineno()
     return lineno
-
+      
 ##The following three functions are written to HTML to achieve visual purposes.
 def notget():
     print('notget is running')
@@ -94,19 +94,23 @@ urliwant='https://www.comap-math.com/mcm/2023Certs/2300368.pdf'
 
 while True:
     print('is running')
-    YorNhave=requests.get(urlhave,headers=headers,timeout=5)
+    YorNhave=requests.get(urlhave,headers=headers,timeout=10)
     print(YorNhave)
-    if str(YorNhave)=='<Response [200]>': ##Determine whether the network is unobstructed
+    YorNhave.close
+    if str(YorNhave)=='<Response [200]>':##Determine whether the network is unobstructed
+        print('is running 2')
         for i in range(5):
-             time.sleep(110)
-             time.sleep(20*random.random())
-             res=requests.get(urliwant)
+             res=requests.get(urliwant,timeout=20)
              print(res)
+             res.keep_alive = False
+             res.close
              if str(res)=='<Response [404]>':
              ##Check whether it can be downloaded according to the team control number, and if it returns 404 (Page not found), it is not available for download.
                    notget()
+                   print('notget is finished, then will sleep about 2 mins')
                    time.sleep(110)
                    time.sleep(20*random.random())
+                   print('awake')
              else:
                     haveget()
                     time.sleep(110)
